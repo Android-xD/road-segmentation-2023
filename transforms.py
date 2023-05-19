a1ca710af2890e72d1edf4d6912914dccd0422af
Andri Horat <horatan@student.ethz.ch>
2023-05-19 16:57:41 +0200

    perlin noise in HSV color space

--- transforms.py
+++ transforms.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import random
 import numpy as np
 import torch
+from perlin2d import generate_perlin_noise_2d
 random.seed(0)
 
 
@@ -63,3 +64,74 @@ class GeometricTransform:
         self.translate = [random.random() * self.max_trans_x, random.random() * self.max_trans_y]
         self.scale = random.uniform(self.min_scale, self.max_scale)
         self.shear = random.random()*self.max_shear
+
+import matplotlib.pyplot as plt
+
+class AddPerlinNoise:
+    def __init__(self, res=8):
+        self.res = res
+
+    def __call__(self, tensor):
+        h, w = tensor.shape[-2:]
+        ph, pw = map(lambda h: int(np.ceil(h / self.res)) * self.res, [h, w])
+
+        noise = np.array([generate_perlin_noise_2d((ph, pw),(self.res, self.res)) for i in range(3)])
+        #noise += np.array([generate_perlin_noise_2d((ph, pw), (2*self.res, 2*self.res)) for i in range(3)])
+        noise = noise[:, :h, :w]
+        noise -= np.mean(noise)
+        noise[0] = 0.1 * noise[0]
+        noise[1] = 0.1 * noise[1]
+        noise[2] = 10 * noise[2]
+
+        color_noise = torch.tensor(noise, dtype=float)
+
+        tensor = rgb2hsv_torch(tensor.unsqueeze(0).to(torch.float))
+        tensor = tensor.to(float) + color_noise
+
+
+        tensor = hsv2rgb_torch(tensor)
+        tensor = tensor.squeeze(0)
+        tensor = torch.clip(tensor, 0, 255)
+        return tensor.to(torch.uint8)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+
+
+
+
+def rgb2hsv_torch(rgb: torch.Tensor) -> torch.Tensor:
+    """https://github.com/limacv/RGB_HSV_HSL/blob/master/color_torch.py"""
+    cmax, cmax_idx = torch.max(rgb, dim=1, keepdim=True)
+    cmin = torch.min(rgb, dim=1, keepdim=True)[0]
+    delta = cmax - cmin
+    hsv_h = torch.empty_like(rgb[:, 0:1, :, :])
+    cmax_idx[delta == 0] = 3
+    hsv_h[cmax_idx == 0] = (((rgb[:, 1:2] - rgb[:, 2:3]) / delta) % 6)[cmax_idx == 0]
+    hsv_h[cmax_idx == 1] = (((rgb[:, 2:3] - rgb[:, 0:1]) / delta) + 2)[cmax_idx == 1]
+    hsv_h[cmax_idx == 2] = (((rgb[:, 0:1] - rgb[:, 1:2]) / delta) + 4)[cmax_idx == 2]
+    hsv_h[cmax_idx == 3] = 0.
+    hsv_h /= 6.
+    hsv_s = torch.where(cmax == 0, torch.tensor(0.).type_as(rgb), delta / cmax)
+    hsv_v = cmax
+    return torch.cat([hsv_h, hsv_s, hsv_v], dim=1)
+
+
+def hsv2rgb_torch(hsv: torch.Tensor) -> torch.Tensor:
+    """https://github.com/limacv/RGB_HSV_HSL/blob/master/color_torch.py"""
+    hsv_h, hsv_s, hsv_l = hsv[:, 0:1], hsv[:, 1:2], hsv[:, 2:3]
+    _c = hsv_l * hsv_s
+    _x = _c * (- torch.abs(hsv_h * 6. % 2. - 1) + 1.)
+    _m = hsv_l - _c
+    _o = torch.zeros_like(_c)
+    idx = (hsv_h * 6.).type(torch.uint8)
+    idx = (idx % 6).expand(-1, 3, -1, -1)
+    rgb = torch.empty_like(hsv)
+    rgb[idx == 0] = torch.cat([_c, _x, _o], dim=1)[idx == 0]
+    rgb[idx == 1] = torch.cat([_x, _c, _o], dim=1)[idx == 1]
+    rgb[idx == 2] = torch.cat([_o, _c, _x], dim=1)[idx == 2]
+    rgb[idx == 3] = torch.cat([_o, _x, _c], dim=1)[idx == 3]
+    rgb[idx == 4] = torch.cat([_x, _o, _c], dim=1)[idx == 4]
+    rgb[idx == 5] = torch.cat([_c, _o, _x], dim=1)[idx == 5]
+    rgb += _m
+    return rgb
