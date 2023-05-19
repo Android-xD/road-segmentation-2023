a15b28dc26814e08935e65723bca4cc30253f10d
Andri Horat <horatan@student.ethz.ch>
2023-05-19 14:32:16 +0200

    test if padding improves accuracy

--- eval.py
+++ eval.py
@@ -9,6 +9,7 @@ import torchvision.transforms as T
 import seg_net_lite
 from deeplabv3 import createDeepLabv3,load_model
 from sklearn.metrics import f1_score, accuracy_score
+import torch.nn.functional as F
 
 torch.manual_seed(0)
 
@@ -56,16 +57,51 @@ if __name__ == '__main__':
         pin_memory=True
     )
 
-    model, preprocess = createDeepLabv3(2, 400)
+    model, preprocess = createDeepLabv3(2, 512)
     state_dict = torch.load("out/model_best.pth.tar", map_location=torch.device("cpu"))
     model.load_state_dict(state_dict)
+
+    import torch.nn as nn
+    import torchvision.transforms as T
+    pad = nn.ReflectionPad2d(56)
+    crop = T.CenterCrop(400)
 
     for i, (input, target) in enumerate(val_loader):
         # Move input and target tensors to the device (CPU or GPU)
         input = input.to(device)
+        #print(input.shape, input.dtype)
+        #input = input.squeeze()
+        input = pad(input.squeeze().to(float)).to(torch.uint8)
+        #print(input.shape, input.dtype)
         target = target.to(device)
         output = model(preprocess(input))['out']
-        for j in range(target.shape[0]):
-            vis.output_target_heat(input.detach()[j] / 255, output.detach()[j, 1], 0.3,target[j])
-        print(torch.count_nonzero(target==output[:,1].round())/target.numel())
 
+        output = crop(output)
+        input = crop(input)
+        # normalize the output
+        output = F.softmax(output)
+        #for j in range(target.shape[0]):
+            #vis.output_target_heat(input.detach()[j] / 255, output.detach()[j, 1], 0.3, target[j])
+            #plt.imshow(output[j, 1].detach().cpu().numpy())
+            #plt.show()
+        print(torch.count_nonzero(target == (output[:, 1:2] > 0.5))/target.numel())
+
+"""
+# with pad
+tensor(0.9248)
+tensor(0.9220)
+tensor(0.9046)
+tensor(0.9250)
+tensor(0.9346)
+tensor(0.9063)
+tensor(0.9157)
+
+#  no pad
+tensor(0.9274)
+tensor(0.9220)
+tensor(0.9076)
+tensor(0.9255)
+tensor(0.9352)
+tensor(0.9055)
+tensor(0.9172)
+"""
