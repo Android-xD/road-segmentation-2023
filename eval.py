71e71aa984a7d2317d44aff1891e7a666aba8d4d
Andri Horat <horatan@student.ethz.ch>
2023-06-30 17:25:04 +0200

    visualize 3 output network outputs

--- eval.py
+++ eval.py
@@ -10,6 +10,8 @@ import seg_net_lite
 from deeplabv3 import createDeepLabv3,load_model
 from sklearn.metrics import f1_score, accuracy_score
 import torch.nn.functional as F
+from mask_to_submission import mask_to_submission_strings
+from utils import aggregate_tile
 
 torch.manual_seed(0)
 
@@ -28,8 +30,11 @@ def eval(model, dataset:CustomImageDataset, num_samples=10):
 
 
 
+
+
+
 if __name__ == '__main__':
-    dataset_path = r"./data/test_set_images"
+    test_set = r"./data/test/images"
     training_set = r"./data/training"
 
     # Check if GPU is available
@@ -38,10 +43,9 @@ if __name__ == '__main__':
     # Define the device to be used for computation
     device = torch.device("cuda" if use_cuda else "cpu")
 
-    dataset = CustomImageDataset(training_set)
+    dataset = CustomImageDataset(training_set, test=True)
 
     train_dataset, val_dataset = test_train_split(dataset, 0.8)
-
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=4,
@@ -57,40 +61,46 @@ if __name__ == '__main__':
         pin_memory=True
     )
 
-    model, preprocess = createDeepLabv3(2, 400)
+    model, preprocess = createDeepLabv3(3, 400)
     state_dict = torch.load("out/model_best.pth.tar", map_location=torch.device("cpu"))
     model.load_state_dict(state_dict)
 
     for i, (input, target) in enumerate(val_loader):
+
         # Move input and target tensors to the device (CPU or GPU)
         input = input.to(device)
         input = input.squeeze()
         target = target.to(device)
         output = model(preprocess(input))['out']
         # normalize the output
-        output = F.softmax(output)
+        pred = output[:, :1]
+        sdf = output[:, 1:2]
+        width = output[:, 2:3]/6
+        pred = F.sigmoid(pred)
+        sdf = F.sigmoid(sdf)
+        width = F.relu6(width)
         for j in range(target.shape[0]):
-            vis.output_target_heat(input.detach()[j] / 255, output.detach()[j, 1], 0.3, target[j])
-            plt.imshow(output[j, 1].detach().cpu().numpy())
-            plt.show()
-        print(torch.count_nonzero(target == (output[:, 1:2] > 0.5))/target.numel())
+            for out in [pred, sdf, width]:
+                vis.output_target_heat(input.detach()[j] / 255, out.detach()[j], 0.3, target[j,:1])
+                plt.imshow(out[j, 0].detach().cpu().numpy())
+                plt.show()
 
-"""
-# with pad
-tensor(0.9248)
-tensor(0.9220)
-tensor(0.9046)
-tensor(0.9250)
-tensor(0.9346)
-tensor(0.9063)
-tensor(0.9157)
+    target = aggregate_tile(target.to(float))
 
-#  no pad
-tensor(0.9274)
-tensor(0.9220)
-tensor(0.9076)
-tensor(0.9255)
-tensor(0.9352)
-tensor(0.9055)
-tensor(0.9172)
-"""
+    pred = (output > 0.2)
+    pred_string = mask_to_submission_strings(pred)
+
+    pred = aggregate_tile(pred*1.0)
+    #cv2.imwrite(f"prediction/{}")
+
+    plt.imshow(pred[0,0] * 1.0)
+    plt.show()
+    plt.imshow(target[0,0] * 1.0)
+    plt.show()
+    """accuracy = torch.count_nonzero(target == pred)/target.numel()
+    TP = torch.count_nonzero(target[1 == pred])
+    recall = TP / torch.count_nonzero(target)
+    precision = TP / torch.count_nonzero(pred)
+    f1 = 2./(1/recall + 1/precision)
+    print(accuracy, recall, precision, f1)"""
+
