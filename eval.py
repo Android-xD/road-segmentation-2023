6d22689362d8c21f60e3f70169977c286a303d90
Andri Horat <horatan@student.ethz.ch>
2023-07-06 22:13:56 +0200

    non minima supression and direction propagation

--- eval.py
+++ eval.py
@@ -28,9 +28,94 @@ def eval(model, dataset:CustomImageDataset, num_samples=10):
             img = dataset.affineTransform.backward(img)
             vis.show_img_mask(img.detach(), output.detach())
 
+def nms(img, dir=None ):
+    if dir is None:
+        img = cv2.GaussianBlur(img,ksize=(15,15),sigmaX=5)
+        dx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+        dy = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+        dir = (-np.arctan2(dy, dx)) % np.pi / np.pi
+        gradient_magnitude = (dx ** 2 + dy ** 2) ** 0.5
+    #else:
+        #dir =cv2.GaussianBlur(dir,ksize=(15,15),sigmaX=5)
 
+    angle_quantization = 4  # Number of discrete directions
 
+    quantized_angle = np.round(dir*angle_quantization)
 
+    suppressed = np.zeros_like(img)
+    height, width = img.shape
+    r = 4
+    ri = np.array([i_ for i_ in range(-r, r + 1) if i_ != 0])
+    for i in range(0, height):
+        for j in range(0, width):
+            if img[i, j] <= 0.1:
+                suppressed[i, j] = 0
+            else:
+                direction = quantized_angle[i, j]
+                if direction == 0:
+                    idx_i, idx_j = i, j + ri
+                elif direction == 1:
+                    idx_i, idx_j = i + ri, j + ri
+                elif direction == 2:
+                    idx_i, idx_j = i + ri, j
+                else:
+                    idx_i, idx_j = i + ri, j - ri
+                idx_i, idx_j = np.clip(idx_i, 0, height-1), np.clip(idx_j, 0, width-1)
+                neighbors = img[idx_i, idx_j]
+
+                if img[i, j] <= np.max(neighbors):
+                    suppressed[i, j] = 1
+                else:
+                    suppressed[i, j] = 0
+
+    return suppressed
+
+def prop(img,dir):
+    if dir is None:
+        img = cv2.GaussianBlur(img,ksize=(15,15),sigmaX=5)
+        dx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+        dy = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+        dir = (-np.arctan2(dy, dx)) % np.pi / np.pi
+        gradient_magnitude = (dx ** 2 + dy ** 2) ** 0.5
+    #else:
+        #dir =cv2.GaussianBlur(dir,ksize=(15,15),sigmaX=5)
+
+    angle_quantization = 4  # Number of discrete directions
+
+    quantized_angle = np.round(dir*angle_quantization)
+
+    suppressed = np.zeros_like(img)
+    height, width = img.shape
+    r = 4
+    p = np.zeros((5,*img.shape))
+    for it in range(5):
+        ri = np.array([i_ for i_ in range(-r, r + 1) if i_ != 0])
+        for i in range(0, height):
+            for j in range(0, width):
+                if img[i, j] == 0:
+                    suppressed[i, j] = 0
+                else:
+                    direction = (quantized_angle[i, j] + 2 )% 4
+                    if direction == 0:
+                        idx_i, idx_j = i, j + ri
+                    elif direction == 1:
+                        idx_i, idx_j = i + ri, j + ri
+                    elif direction == 2:
+                        idx_i, idx_j = i + ri, j
+                    else:
+                        idx_i, idx_j = i + ri, j - ri
+                    idx_i, idx_j = np.clip(idx_i, 0, height-1), np.clip(idx_j, 0, width-1)
+                    neighbors = img[idx_i, idx_j]
+
+                    suppressed[i, j] = np.mean(neighbors)
+        img = suppressed
+        p[it]=suppressed
+    # while(True):
+    #     for it in range(5):
+    #         cv2.imshow("",p[it])
+    #         cv2.waitKey(100)
+
+    return suppressed
 
 
 if __name__ == '__main__':
@@ -103,12 +188,14 @@ if __name__ == '__main__':
             #    out[x] = cv2.medianBlur(w, ksize=15)/255.
             out[5] = out[1] < out[2]*0.5
 
-            vis.direction_field(out[3],out[0],8)
+            #vis.direction_field(out[3], out[0],8)
             images = [img]+out
             names = ["img", "probability", "signed distance","width", "direction", "patch prediction", "final prediction", "gt prediction"]
-            plot_images(images, names)
+            #plot_images(images, names)
+            plot_images([img[:, :, 0] + prop(out[0], out[3]), out[3]])
             #plt.show()
-            plt.savefig(f"./figures/out_{i}_{j}.jpg")
+            plt.show()
+            #plt.savefig(f"./figures/out_{i}_{j}.jpg")
 
     target = aggregate_tile(target.to(float))
 
