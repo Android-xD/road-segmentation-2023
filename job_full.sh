5af973db9de98ff020da607641370454c2280477
Andri Horat <horatan@eu-login-27.euler.ethz.ch>
2023-07-28 19:21:53 +0200

    train pipeline

--- job_full.sh
+++ job_full.sh
@@ -10,11 +10,11 @@
 #SBATCH --open-mode=truncate # to overrides out and err files, you can also use
 
 source startup.sh
-python train.py --data ./data_google/training --model fpn --epochs 30 --full True --augmentations True
-python train.py --data ./data/training --model fpn --epochs 30 --full True --augmentations True --load_model ./out/model_best.pth.tar
-python train.py --data ./data_google/training --model fpn --epochs 10 --full True --augmentations False --load_model ./out/model_best.pth.tar
-python train.py --data ./data/training --model fpn --epochs 10 --full True --augmentations False --load_model ./out/model_best.pth.tar
-python submit.py --model fpn --n_samples 100 --threshold 0.35
+python train.py --data './data_google/training' --model 'fpn' --epochs 30 --full True --augmentations True
+python train.py --data './data/training' --model 'fpn' --epochs 30 --full True --augmentations True --load_model './out/model_best.pth.tar'
+python train.py --data './data_google/training' --model 'fpn' --epochs 10 --full True --augmentations False --load_model './out/model_best.pth.tar'
+python train.py --data './data/training' --model 'fpn' --epochs 10 --full True --augmentations False --load_model './out/model_best.pth.tar'
+python submit.py --model 'fpn' --n_samples 100 --threshold 0.35
 python mask_to_submission.py
 
 python train
