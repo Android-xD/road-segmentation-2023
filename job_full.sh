1165abe9f80f0f385f5ea4d47dc0bb59bd8dd70d
Andri Horat <horatan@student.ethz.ch>
2023-07-29 21:23:36 +0200

    add train script for best model

--- job_full.sh
+++ job_full.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-#SBATCH -n 4
-#SBATCH --time=10:00:00
+#SBATCH -n 16
+#SBATCH --time=24:00:00
 #SBATCH --gpus=1
-#SBATCH --mem-per-cpu=4000
-#SBATCH --tmp=4000 # per node!!
+#SBATCH --mem-per-cpu=4096
+#SBATCH --tmp=4096 # per node!!
 #SBATCH --job-name=cil_train
 #SBATCH --output=cil_train.out # specify a file to direct output stream
 #SBATCH --error=paperX.err
 #SBATCH --open-mode=truncate # to overrides out and err files, you can also use
 
 source startup.sh
-python train.py --data './data_google/training' --model 'fpn' --epochs 30 --full True --augmentations True
-python train.py --data './data/training' --model 'fpn' --epochs 30 --full True --augmentations True --load_model './out/model_best.pth.tar'
-python train.py --data './data_google/training' --model 'fpn' --epochs 10 --full True --augmentations False --load_model './out/model_best.pth.tar'
-python train.py --data './data/training' --model 'fpn' --epochs 10 --full True --augmentations False --load_model './out/model_best.pth.tar'
-python submit.py --model 'fpn' --n_samples 100 --threshold 0.35
+python train.py --lr 1.e-4 --data './data_google/training' --model 'fpn' --epochs 30 --full False --augmentations True
+python train.py --lr 1.e-4 --data './data/training' --model 'fpn' --epochs 30 --full False --augmentations True --load_model './out/model_best.pth.tar'
+python submit.py --model 'fpn' --n_samples 50 --threshold 0.35
 python mask_to_submission.py
-
-python train
